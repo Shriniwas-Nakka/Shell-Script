93408ef9c3f459105ef109ff6953409c72fc6d71
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-09 17:16:41 +0530

    Adding daily wage to the program

--- Shell script/employeeWageFinal.sh
+++ Shell script/employeeWageFinal.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -x
-x=$(( RANDOM %2 ))
-if [ $x == 1 ];
-then
-echo employee present
-else
-echo employee absent 
-fi
+x=$(( RANDOM %3 ))
+   if [ $x -eq 1 ];
+      then
+      rate=20;
+      hour=8;
+      salary=$(( $rate * $hour ));
+      echo "Employee present"
+      echo "Monthly salary" $salary
+   elif [ $x -eq 2 ]
+      then
+      echo "parttime worker"
+      parttimerate=10;
+      hour=4
+      salary=$(( $parttimerate * $hour ));
+      echo $salary
+   else
+      salary=0;
+      echo "Employee absent" 
+   fi
