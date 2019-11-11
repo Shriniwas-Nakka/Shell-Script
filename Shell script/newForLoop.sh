8c12c086755880e77c6fed5c1b1489b1e920c9cb
suraj7852 <surajpj7852@gmail.com>
2019-11-11 10:59:32 +0530

    function program example

--- Shell script/newForLoop.sh
+++ Shell script/newForLoop.sh
@@ -1,6 +1,25 @@
 #!/bin/bash -x
-for (( day=1; day<=20; day++ ))
+isPartTime=1;
+isFullTime=2;
+rate=20;
+#employeeCheck=$(( RANDOM %3 ))
+for (( counter=1; counter<=20; counter++ ))
 do
-	echo $day
+   employeeCheck=$(( RANDOM %3 ))
+   if [ $isFullTime -eq $employeeCheck ]
+   then
+   hour=8;
+   elif [ $isPartTime -eq $employeeCheck ]
+   then
+   hour=4
+   else
+   hour=0
+   fi
+#done
+perDaySalary=$(( $hour * $rate ))
+monthlySalary=$(( $monthlySalary + $perDaySalary ))
 done
+totalSalary=$monthlySalary
+
+
 
