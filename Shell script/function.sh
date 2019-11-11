037e24ee0fc04bb758f8aa430224ee1fea3aee9f
suraj7852 <surajpj7852@gmail.com>
2019-11-11 11:20:22 +0530

    function program total hours calculated

--- Shell script/function.sh
+++ Shell script/function.sh
@@ -3,10 +3,23 @@ function myFunc() {
 	echo $1
 }
 
-result="$( myFunc $(( RANDOM%2 )) )"
-if [ $result -eq 1 ]
-then
-	echo "Success"
-else
-	echo "Failure"
-fi
+result="$( myFunc $(( RANDOM%3 )) )"
+isFullTime=1
+isPartTime=2
+empHr=20
+for (( counter=1; counter<=20; counter++ ))
+do
+	result="$( myFunc $(( RANDOM%3 )) )"
+	if [ $result -eq $isFullTime ]
+	then
+		hour=4;
+		#echo "Success"
+	elif [ $result -eq $isPartTime ]
+	then
+		hour=2;
+		#echo "Failure"
+	else
+		hour=0;
+	fi
+totalHour=$(( $hour + $empHr ))
+done
