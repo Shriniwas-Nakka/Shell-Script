2845b113d361ac50628f6d8f40a8fee534cee130
suraj7852 <surajpj7852@gmail.com>
2019-11-11 11:40:25 +0530

    updated function program for calculating total employee hour

--- Shell script/function.sh
+++ Shell script/function.sh
@@ -1,25 +1,27 @@
 #!/bin/bash -x
+isFullTime=1;
+isPartTime=2;
+empRatePerHr=20;
+workingDays=20;
+
+totalWorkingHours=0;
+totalWorkingDays=0;
+
 function myFunc() {
-	echo $1
+	#echo $1
+	case $1 in
+	$isFullTime)
+		hour=8;;
+	$isPartTime)
+		hour=4;;
+	*)
+		hour=0;;
+	esac
+	totalWorkingHour=$(( $hour + $empRatePerHr ))
 }
 
-result="$( myFunc $(( RANDOM%3 )) )"
-isFullTime=1
-isPartTime=2
-empHr=20
+#result="$( myFunc $(( RANDOM%3 )) )"
 for (( counter=1; counter<=20; counter++ ))
 do
 	result="$( myFunc $(( RANDOM%3 )) )"
-	if [ $result -eq $isFullTime ]
-	then
-		hour=4;
-		#echo "Success"
-	elif [ $result -eq $isPartTime ]
-	then
-		hour=2;
-		#echo "Failure"
-	else
-		hour=0;
-	fi
-totalHour=$(( $hour + $empHr ))
 done
