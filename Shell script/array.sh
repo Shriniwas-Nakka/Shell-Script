5ce5e7a4b46ee5f6df00d42f6efa2fd0e48b95c3
suraj7852 <surajpj7852@gmail.com>
2019-11-11 13:21:12 +0530

    array program adding daily wages to array

--- Shell script/array.sh
+++ Shell script/array.sh
@@ -1,7 +1,38 @@
 #!/bin/bash -x
-counter=0;
-Fruits[((counter++))]="Apple"
-Fruits[((counter++))]="banana"
-Fruits[((counter++))]="Orange"
+isFullTime=1;
+isPartTime=2;
+empRatePerHr=20;
+workingDays=20;
+count=0;
 
-echo ${Fruits[@]}
+totalWorkingHours=0;
+totalWorkingDays=0;
+
+function dailyHour() {
+        case $1 in
+        $isFullTime)
+                hour=8;;
+        $isPartTime)
+                hour=4;;
+        *)
+                hour=0;;
+        esac
+	echo $hour
+}
+
+function wages() {
+	TotalWages=$(( $1 * $empRatePerHr ))
+	echo $TotalWages
+}
+
+for (( counter=1; counter<=20; counter++ ))
+do
+        workHour="$( dailyHour $(( RANDOM%3 )) )"
+	totalWorkingHours=$(( $totalWorkingHours + $workHour ))
+	wage="$( wages $(( $totalWorkingHours )) )"
+	Wages[(( counter++ ))]=$wage
+	totalWages=$(( $totalWages + $wage ))
+	echo $totalWages
+done
+Wages[(( counter++ ))]=$totalWages
+echo ${Wages[@]}
